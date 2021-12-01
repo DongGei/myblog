643e8ff9c080afabb1078f0f7663c43d87e8284e
dong2645981073 <2645981073@qq.com>
2021-12-01 09:00:14 +0800

    Site updated: 2021-12-01 09:00:13

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -3,13 +3,13 @@ var OriginTitle = document.title;
 var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
-        $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/Yafine/cdn@2.5/source/favicon.png");
-        document.title = 'ヽ(●-`Д´-)ノ你要玩捉迷藏嘛';
+        $('[rel="icon"]').attr('href', "https://gitee.com/dong2645981073/picture-summary/raw/master//image/image-20211128171544646.png");
+        document.title = '我相信你还会回来的！';
         clearTimeout(titleTime);
     }
     else {
-        $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/Yafine/cdn@2.5/source/favicon.png");
-        document.title = 'ヾ(Ő∀Ő3)ノ好哦！' + OriginTitle;
+        $('[rel="icon"]').attr('href', "https://gitee.com/dong2645981073/picture-summary/raw/master//image/image-20211128171544646.png");
+        document.title = '哈哈，我就知道！' + OriginTitle;
         titleTime = setTimeout(function () {
             document.title = OriginTitle;
         }, 2000);
